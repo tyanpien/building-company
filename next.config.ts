f43c72607f7b2a11853038ac65e0381f71178ce9
Настя Кудря <nastakudra44574@gmail.com>
2025-07-05 22:13:46 +0600

    сайт

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
   images: {
-    domains: [], // add any external domains if you use external images
-    unoptimized: true, // if you don't need Next.js image optimization
+    domains: [],
+    unoptimized: true,
+    output: 'standalone',
   },
 };
 
